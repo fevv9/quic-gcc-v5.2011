ec780fda0c3b3c7b95c78e1dcbf03e1baa1238db
sundeepk <>
2010-01-27 22:25:39 +0000

    V4 Merge - Merge from STABLE_V4 branch. Formatting and vector support.

--- gcc/function.c
+++ gcc/function.c
@@ -4804,7 +4804,7 @@ expand_function_end (void)
 
     start_sequence ();
     clobber_return_register ();
-    //expand_naked_return ();
+    /* expand_naked_return (); */
     seq = get_insns ();
     end_sequence ();
 
