e41f7ac5e54275f0bc3d9c714736484d651dfdee
spande <>
2010-09-29 16:19:05 +0000

    update for building with newlib - bug # 4486

--- gcc/config/qdsp6/newlib.h
+++ gcc/config/qdsp6/newlib.h
@@ -2,6 +2,6 @@
 Controlling the Compilation Driver, gcc
 -------------------------------------*/
 
-/* Link crt0 first. */
+/* Link crt0_standalone first. */
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+#define STARTFILE_SPEC "crt0_standalone.o%s startmain.o%s crt0.o%s crti.o%s crtbegin.o%s"
