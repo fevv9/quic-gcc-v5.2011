13768cf7c1bde06341688212734d8f584223e827
Sundeep Kushwaha <sundeepk@quicinc.com>
2011-01-14 11:38:51 -0600

    newlib build

--- gcc/config/hexagon/hexagon.c
+++ gcc/config/hexagon/hexagon.c
@@ -2221,11 +2221,11 @@ hexagon_init_libfuncs(void)
 
   if (TARGET_V5_FEATURES)
     {
-      set_optab_libfunc(sdiv_optab, SFmode, "__hexagon_divsf3_opt");
-      set_optab_libfunc(sdiv_optab, DFmode, "__hexagon_divdf3_opt");
+      set_optab_libfunc(sdiv_optab, SFmode, "__qdsp_divsf3_opt");
+      set_optab_libfunc(sdiv_optab, DFmode, "__qdsp_divdf3_opt");
 
-      set_optab_libfunc(sqrt_optab, SFmode, "__hexagon_sqrtsf_opt");
-      set_optab_libfunc(sqrt_optab, DFmode, "__hexagon_sqrtdf_opt");
+      set_optab_libfunc(sqrt_optab, SFmode, "__qdsp_sqrtsf_opt");
+      set_optab_libfunc(sqrt_optab, DFmode, "__qdsp_sqrtdf_opt");
     }
   
   if (0 && flag_unsafe_math_optimizations)
