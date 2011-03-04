31833543eeccd045add834698e832d22f954f2b7
Linas Vepstas <c_linasv@quicinc.com>
2011-03-04 09:53:18 -0600

    Use pcrel relocations in exception frames
    Make sure the -fpic also uses pcrel relocations in eh_frame's.
    This patch also makes a distinction between global and non-global
    refs, and also between data and text; it treats these in more-or-less
    the same way that other arches do.

--- gcc/config/hexagon/hexagon.h
+++ gcc/config/hexagon/hexagon.h
@@ -652,8 +652,15 @@ Exception Handling Support
 #define EH_RETURN_HANDLER_RTX \
   gen_rtx_MEM (Pmode, plus_constant(hard_frame_pointer_rtx, UNITS_PER_WORD))
 
+/* Select a format to encode pointers in exception handling data.  CODE
+   is 0 for data, 1 for code labels, 2 for function pointers.  GLOBAL is
+   true if the symbol may be affected by dynamic relocations.
+ */
 #define ASM_PREFERRED_EH_DATA_FORMAT(CODE, GLOBAL) \
-  (flag_pic  ? DW_EH_PE_aligned : DW_EH_PE_absptr)
+  ((flag_pic  ?  \
+    (((GLOBAL) ? DW_EH_PE_indirect : 0) | DW_EH_PE_pcrel) \
+       : DW_EH_PE_absptr) | \
+  ((CODE) ? 0 : DW_EH_PE_sdata4))
 
 /* ??? do we want this? */
 /*#define ASM_MAYBE_OUTPUT_ENCODED_ADDR_RTX(FILE, ENCODING, SIZE, ADDR, DONE)*/
